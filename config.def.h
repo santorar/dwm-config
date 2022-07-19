9f6f4fc7650be4349b836968762e3bb4b2c77b01
santorar <estractor179@gmail.com>
2022-07-19 10:41:55 -0500

    resolved the conflicts

--- config.def.h
+++ config.def.h
@@ -68,7 +68,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const scratchpad qalculate = {.class = "Qalculate-gtk", .v = (char *[]){"qalculate-gtk", NULL}};
+static const scratchpad qalculate = {.class = "st", .v = (char *[]){"st", NULL}};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
