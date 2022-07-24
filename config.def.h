d1e61c5d9987953ce2a8fe03e4eaa5881e2c0656
santorar <estractor179@gmail.com>
2022-07-23 22:26:17 -0500

    removed status2d

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 2;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "monospace:pixelsize=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
 static const char dmenufont[]       = "UbuntuMono Nerd Font:size=11";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#504945";
